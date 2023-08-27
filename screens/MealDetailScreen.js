71eab553d9dc4427786ad7054eca30a84997c4c4
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-08-26 23:51:08 -0300

    feat: dinamic page of meal details

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,29 @@
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import { MealDetails } from "../components/MealDetails";
 
-export function MealDetailScreen({route}) {
-    const mealId = route.params.mealId
-    return(
-        <Text>This is meal {mealId}</Text>
-    )
+export function MealDetailScreen({ route }) {
+  const mealId = route.params.mealId;
+
+  const selectMeal = MEALS.find((meal) => meal.id === mealId);
+  return (
+    <View>
+      <Image source={{ url: selectMeal.imageUrl }}></Image>
+      <Text>{selectMeal.title}</Text>
+      <MealDetails
+        duration={selectMeal.duration}
+        complexity={selectMeal.complexity}
+        affordability={selectMeal.affordability}
+      />
+      <View></View>
+      <Text>Ingredients</Text>
+      {selectMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+
+      <Text>Steps</Text>
+      {selectMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>))}
+    </View>
+  );
 }
